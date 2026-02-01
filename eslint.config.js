c9e08df08d288a5d9b1c9d456faabeb8224b7c8f
Martin Tracey <matracey@outlook.ie>
2026-01-31 17:15:07 -0800

    chore: add eslint with eslint.config file

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default [
+  {
+    files: ["src/**/*.js", "e2e/**/*.js", "*.js"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      curly: ["error", "all"],
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            // External packages (node_modules)
+            ["^[a-z@]"],
+            // Internal imports (relative paths)
+            ["^\\."],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+  },
+];
