080110d0d5a924215ccfc23966676d69b6094a53
Martin Tracey <matracey@outlook.ie>
2026-02-14 05:22:36 -0800

    feat(app): add shell components barrel export

---
+++ src/components/react/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Shell component barrel exports.
+ * @module components/react
+ */
+
+export { Header } from "./Header";
+export type { HeaderProps } from "./Header";
+
+export { Sidebar } from "./Sidebar";
+export type { SidebarProps } from "./Sidebar";
+
+export { Flags } from "./Flags";
+export type { FlagsProps } from "./Flags";
+
+export { NavButtons } from "./NavButtons";
+export type { NavButtonsProps } from "./NavButtons";
